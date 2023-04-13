31563bcc77075231f8601e72babb31a766855d75
ServEr <Susurkaev@yandex.ru>
2023-04-13 18:06:56 +0300

    Break added

---
+++ Конструкция Break/main.py
@@ -0,0 +1,9 @@
+name = None
+
+while True:
+    #print('Ответ не верный. Повторите попытку: ')
+    name = input('Кто создатель Python?: ')
+    if name == 'Гвидо':
+        break
+    print('Не верно')
+print('Вы ответили верно!')
